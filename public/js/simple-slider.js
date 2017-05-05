d09b7f5b8ac05ace3c98ef3f71c089e4c3f8ae1a
lankovova <lanko.vova@gmail.com>
2017-05-06 01:49:04 +0300

    refactoring

--- public/js/simple-slider.js
+++ public/js/simple-slider.js
@@ -1,42 +1,46 @@
 $(function() {
 
 	var firstSlider = new SimpleSlider($('.first-slider-wrapper'));
-	// firstSlider.setSize();
 
 	var secondSlider = new SimpleSlider($('.second-slider-wrapper'));
-	// secondSlider.setSize();
 
 });
 
 // Constructor for SimpleSlider class
-function SimpleSlider(sliderWrap) {
-	this.slider = sliderWrap.find('.slider');
+function SimpleSlider(userWrap) {
+	this.slider = userWrap.find('.slider');
 	this.slides = this.slider.find('.slides');
 	this.slide = this.slides.find('.slide');
 
 	this.amountOfSlides = this.slide.length;
 	this.currentSlide = 0;
 
+	// Init slider size
 	this.setSize();
 
+	// Slide left
 	this.slider.find('#left').click(() => {
 		(this.currentSlide === 0) ? this.slideTo(this.amountOfSlides - 1) : this.slideTo(this.currentSlide - 1);
 	});
 
+	// Slide right
 	this.slider.find('#right').click(() => {
 		(this.currentSlide === this.amountOfSlides - 1) ? this.slideTo(0) : this.slideTo(this.currentSlide + 1);
 	});
 
+	// Adaptive sizing
 	$(window).resize(() => {
 		this.setSize();
 	});
 }
 
 SimpleSlider.prototype = {
+	// Slide function
 	slideTo : function(destinationSlide) {
 		this.slides.css({'margin-left': -this.slider.width() * destinationSlide});
 		this.currentSlide = destinationSlide;
 	},
+	// Resize slider
 	setSize : function() {
 		console.log(this);
 		this.slides.width(this.slider.width() * this.slide.length);
